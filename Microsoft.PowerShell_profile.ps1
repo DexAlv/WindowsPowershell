c853660f636a48b0f9b180f405017193f342d570
DexAlv <dexalv435@gmail.com>
2026-08-31 17:28:01 -0600

    First Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+﻿function prompt { $E = [char]27; $user = $env:USERNAME; $currentFolder = Split-Path -Leaf -Path (Get-Location); if ($currentFolder -eq '') { $currentFolder = '\' }; return "$E[36m╭─ 🪟 $user
+$E[36m╰─ $E[34m$currentFolder $E[32m❯$E[0m " }; function ssh { $target = $args -join ' '; [Console]::Title = "⚡ SSH: $target"; & ssh.exe $args; [Console]::Title = "Windows PowerShell" }
